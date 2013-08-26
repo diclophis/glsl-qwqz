c5ac941475fac0c8662e207f56886da9ac555169
Diclophis <diclophis@gmail.com>
2013-08-26 10:08:14 +0000

    add emscripten support

---
+++ platforms/emscripten/main.cpp
@@ -0,0 +1,114 @@
+// Vanilla Linux OpenGL+GLUT+SOIL App
+
+#define GL_GLEXT_PROTOTYPES
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GL/glut.h>
+
+extern "C" {
+  #include "opengles_bridge.h"
+  #include "libqwqz.h"
+}
+
+static int game_index = 0;
+static bool left_down = false;
+static bool right_down = false;
+static bool reset_down = false;
+static bool debug_down = false;
+int kWindowWidth = 0;
+int kWindowHeight = 0;
+static qwqz_handle qwqz_engine = NULL;
+
+void draw(void) {
+  qwqz_draw(qwqz_engine);
+  glutPostRedisplay();
+}
+
+
+void resize(int width, int height) {
+  kWindowWidth = width;
+  kWindowHeight = height;
+  qwqz_resize(qwqz_engine, (float)width, (float)height);
+}
+
+
+void processMouse(int button, int state, int x, int y) {
+  switch (state) {
+    case GLUT_DOWN:
+      //Engine::CurrentGameHit(x, y, 0);
+      break;
+    case GLUT_UP:
+      //Engine::CurrentGameHit(x, y, 2);
+      break;
+  }
+}
+
+
+void processMouseMotion(int x, int y) {
+  //Engine::CurrentGameHit(x, y, 1);
+}
+
+
+void processNormalKeys(unsigned char key, int x, int y) {
+  LOGV("key: %d %c\n", key, key);
+  if (key == 49) {
+    if (debug_down) {
+      //Engine::CurrentGameHit(0, 0, 2);
+    } else {
+      //Engine::CurrentGameHit(0, 0, 0);
+    }
+    debug_down = !debug_down;
+  } else if (key == 110) {
+    if (left_down) {
+      //Engine::CurrentGameHit(0, 1024, 2);
+    } else {
+      //Engine::CurrentGameHit(0, 1024, 0);
+    }
+    left_down = !left_down;
+  } else if (key == 109) {
+    if (right_down) {
+      //Engine::CurrentGameHit(1024, 1024, 2);
+    } else {
+      //Engine::CurrentGameHit(1024, 1024, 0);
+    }
+    right_down = !right_down;
+  } else {
+    if (reset_down) {
+    } else {
+      //Engine::Start(game_index, kWindowWidth, kWindowHeight);
+    }
+    reset_down = !reset_down;
+  }
+}
+
+
+int main(int argc, char** argv) {
+  char *wh;
+  
+  wh = emscripten_run_script_string("(function(){return document.body.offsetWidth;})()");
+  kWindowWidth = atoi(wh);
+
+  wh = emscripten_run_script_string("(function(){return document.body.offsetHeight;})()");
+  kWindowHeight = atoi(wh);
+
+  LOGV("%s %d %d\n", wh, kWindowWidth, kWindowHeight);
+
+  glutInit(&argc,argv);
+  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(kWindowWidth, kWindowHeight);
+  glutCreateWindow("does emscripten GLUT wrapper set window.title?, do I need a title? //TODO");
+  glutKeyboardFunc(processNormalKeys);
+  glutKeyboardUpFunc(processNormalKeys);
+  glutMouseFunc(processMouse);
+  glutMotionFunc(processMouseMotion);
+  glutDisplayFunc(draw);
+  glutReshapeFunc(resize);
+
+  qwqz_engine = qwqz_alloc();
+  qwqz_init(qwqz_engine);
+
+  glutMainLoop();
+
+  return 0;
+}
