f2df581bc5077ed79f5b04009fb700bad23dc425
Diclophis <diclophis@gmail.com>
2014-06-05 21:18:14 -0700

    try protothreads

--- sidescroll-src/sidescroll_main.c
+++ sidescroll-src/sidescroll_main.c
@@ -3,7 +3,6 @@
 //TODO: move this into libqwqz
 #define DEGREES_TO_RADIANS(__ANGLE__) ((__ANGLE__) / 180.0 * M_PI)
 
-
 #include "opengles_bridge.h"
 #include "libqwqz.h"
 #include "impl_main.h"
@@ -15,6 +14,8 @@
 #include <spine/extension.h>
 #include "spine_bridge.h"
 
+/* We must always include pt.h in our protothreads code. */
+#include "pt/pt.h"
 
 //TODO: abstract chipmunk render system into bridge functions
 #define GRABBABLE_MASK_BIT (1<<31)
@@ -136,6 +137,89 @@ static int num_bg = 0;
 static float bg_scale = 4.0;
 static int bg_range = 5;
 
+//protothreads
+static struct pt pt1, pt2;
+/* Two flags that the two protothread functions use. */
+static int protothread1_flag, protothread2_flag;
+
+
+/**
+ * This is a very small example that shows how to use
+ * protothreads. The program consists of two protothreads that wait
+ * for each other to toggle a variable.
+ */
+/**
+ * The first protothread function. A protothread function must always
+ * return an integer, but must never explicitly return - returning is
+ * performed inside the protothread statements.
+ *
+ * The protothread function is driven by the main loop further down in
+ * the code.
+ */
+static int
+protothread1(struct pt *pt)
+{
+  /* A protothread function must begin with PT_BEGIN() which takes a
+     pointer to a struct pt. */
+  PT_BEGIN(pt);
+
+  /* We loop forever here. */
+  while(1) {
+    /* Wait until the other protothread has set its flag. */
+    PT_WAIT_UNTIL(pt, protothread2_flag != 0);
+    LOGV("Protothread 1 running\n");
+
+    /* We then reset the other protothread's flag, and set our own
+       flag so that the other protothread can run. */
+    protothread2_flag = 0;
+    protothread1_flag = 1;
+
+    /* And we loop. */
+  }
+
+  /* All protothread functions must end with PT_END() which takes a
+     pointer to a struct pt. */
+  PT_END(pt);
+}
+
+/**
+ * The second protothread function. This is almost the same as the
+ * first one.
+ */
+static int
+protothread2(struct pt *pt)
+{
+  PT_BEGIN(pt);
+
+  while(1) {
+    /* Let the other protothread run. */
+    protothread2_flag = 1;
+
+    /* Wait until the other protothread has set its flag. */
+    PT_WAIT_UNTIL(pt, protothread1_flag != 0);
+    LOGV("Protothread 2 running\n");
+    
+    /* We then reset the other protothread's flag. */
+    protothread1_flag = 0;
+
+    /* And we loop. */
+  }
+  PT_END(pt);
+}
+
+/**
+ * Finally, we have the main loop. Here is where the protothreads are
+ * initialized and scheduled. First, however, we define the
+ * protothread state variables pt1 and pt2, which hold the state of
+ * the two protothreads.
+ */
+  
+  /*
+   * Then we schedule the two protothreads by repeatedly calling their
+   * protothread functions and passing a pointer to the protothread
+   * state variables as arguments.
+   */
+
 int impl_hit(int x, int y, int s) {
   
   if (s == 0) {
@@ -148,6 +232,10 @@ int impl_hit(int x, int y, int s) {
 }
 
 int impl_draw(int b) {
+  LOGV("impl_draw\n");
+  protothread1(&pt1);
+  protothread2(&pt2);
+
   float source_bg_width = 320.0;
   float source_bg_scale = bg_scale;
   float total_w = source_bg_width * source_bg_scale;
@@ -430,6 +518,11 @@ int impl_main(int argc, char** argv, GLuint b) {
   qwqz_batch_init(&qwqz_engine->m_Batches[1], &qwqz_engine->m_Linkages[1], (skeleton->slotCount));
   
   qwqz_engine->g_lastFrameBuffer = b;
+
+  /* Initialize the protothread state variables with PT_INIT(). */
+  PT_INIT(&pt1);
+  PT_INIT(&pt2);
   
   return 0;
 }
+
