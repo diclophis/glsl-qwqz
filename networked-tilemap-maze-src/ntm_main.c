6a5a3bd664648429e8306bd6712d97f6cec8e3af
Diclophis <diclophis@gmail.com>
2014-08-02 00:05:00 -0700

    add networked tilemap game

---
+++ networked-tilemap-maze-src/ntm_main.c
@@ -0,0 +1,25 @@
+// networked tilemap maze game
+
+#include "opengles_bridge.h"
+#include "libqwqz.h"
+#include "impl_main.h"
+#include <spine/spine.h>
+#include <spine/extension.h>
+#include "spine_bridge.h"
+#include "impl_main.h"
+
+int impl_hit(int x, int y, int s) {
+  return 0;
+}
+
+int impl_draw(int b) {
+  return 0;
+}
+
+int impl_resize(int width, int height, int ew, int eh, int u) {
+  return 0;
+}
+
+int impl_main(int argc, char** argv, GLuint b) {
+  return 0;
+}
