5f0003284b254664667c3449155b27ab42c6d8fc
mavenlink <jon@mavenlink.com>
2014-09-02 23:01:25 -0700

    full character walk 4x walk cycle

--- networked-tilemap-maze-src/ntm_main.c
+++ networked-tilemap-maze-src/ntm_main.c
@@ -17,6 +17,9 @@ static spAnimationStateData* stateData;
 static spAnimationState* state;
 static spRegionAttachment* lastAttachment = NULL;
 static GLuint g_TextureOffset = -1;
+static float flip = -0.0;
+static float offX = 0.0;
+static float offY = 0.0;
 
 // render stuff
 static float verticeBuffer[8] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -38,10 +41,15 @@ float ntm_character_uw(float uw) {
 int impl_hit(int x, int y, int s) {
   float scrh = 2.0 * (qwqz_engine->m_Zoom2 / 1.0);
 
-  skeleton->root->x = (((float)x / qwqz_engine->m_ScreenWidth) * scrh * (qwqz_engine->m_ScreenWidth / qwqz_engine->m_ScreenHeight)) - (0.5 * scrh * (qwqz_engine->m_ScreenWidth / qwqz_engine->m_ScreenHeight));
-  skeleton->root->y = scrh - (((float)y / qwqz_engine->m_ScreenHeight) * scrh);
+  //skeleton->root->x = 
+  offX = (((float)x / qwqz_engine->m_ScreenWidth) * scrh * (qwqz_engine->m_ScreenWidth / qwqz_engine->m_ScreenHeight)) - (0.5 * scrh * (qwqz_engine->m_ScreenWidth / qwqz_engine->m_ScreenHeight));
+  //if (flip) {
+  //  skeleton->root->x *= flip;
+  //}
+  //skeleton->root->y = 
+  offY = ((scrh / 2.0) - (((float)y / qwqz_engine->m_ScreenHeight) * scrh));//  - (0.25 * scrh * ((qwqz_engine->m_ScreenWidth / qwqz_engine->m_ScreenHeight)));
 
-  spSkeleton_updateWorldTransform(skeleton);
+  //spSkeleton_updateWorldTransform(skeleton);
 
   return 0;
 }
@@ -51,20 +59,28 @@ int impl_draw(int b) {
   qwqz_tick_timer(&qwqz_engine->m_Timers[0]);
   glUniform1f(qwqz_engine->m_Linkages[0].g_TimeUniform, qwqz_engine->m_Timers[0].m_SimulationTime);
 
-  float tox = ntm_character_uv((((int)(qwqz_engine->m_Timers[0].m_SimulationTime * (0.333 * 0.5) * 1.0)) % 2));
-  float toy = ntm_character_uw(((int)(qwqz_engine->m_Timers[0].m_SimulationTime * (0.333 * 0.5) * 0.5)) % 26);
-  
-  //tox = 0;
-  //toy = 0;
+  int toxi = (qwqz_engine->m_Timers[0].m_SimulationTime * (0.333 * 0.5) * 1.0);
+  int toyi = (qwqz_engine->m_Timers[0].m_SimulationTime * (0.333 * 0.5) * 0.5);
+  float tox = ntm_character_uv(toxi % 2);
+  float toy = ntm_character_uw(toyi % 26);
 
   int ax = (0 * 4);
-  int ay = ((int)(qwqz_engine->m_Timers[0].m_SimulationTime * 1.0)) % 3;
+  int ay = ((int)(qwqz_engine->m_Timers[0].m_SimulationTime * 1.0)) % 6;
 
-  glUniform2f(g_TextureOffset, tox + ((ntm_character_uv(ax) / 12.0) * 1.0), toy + ((ntm_character_uw(1.0) / 3.0) * ay));
+  int si = ay; //((int)(qwqz_engine->m_Timers[0].m_SimulationTime * 4.0)) % 6;
+  if (si == 1 || si == 3) {
+    flip = 0.0;
+  } else {
+    flip = 1.0;
+  }
+
+  skeleton->flipX = flip; // so simple
+  glUniform2f(g_TextureOffset, tox + ((ntm_character_uv(ax) / 12.0) * 1.0), toy + ((ntm_character_uw(1.0) / 3.0) * (ay % 3)));
 
   spAnimationState_update(state, qwqz_engine->m_Timers[0].step * 1.33);
   spAnimationState_apply(state, skeleton);
   spSkeleton_updateWorldTransform(skeleton);
+
 
   glClearColor(0.01, 1.0, 0.01, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
@@ -75,12 +91,13 @@ int impl_draw(int b) {
 
   spSlot *s = skeleton->drawOrder[0];
   spRegionAttachment *ra = (spRegionAttachment *)s->attachment;
+
   if (s->attachment && s->attachment->type == SP_ATTACHMENT_REGION) {
     if (lastAttachment != ra) {
       lastAttachment = ra;
     }
     
-    spRegionAttachment_computeWorldVertices(ra, 0, 0, s->bone, verticeBuffer);
+    spRegionAttachment_computeWorldVertices(ra, offX, offY, s->bone, verticeBuffer);
     qwqz_batch_add(&qwqz_engine->m_Batches[0], 0, verticeBuffer, NULL, ra->uvs);
   }
 
