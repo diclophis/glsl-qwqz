7bc4737bfeefc787749b19b0a8c27b088e236b3a
Jon Bardin <diclophis@gmail.com>
2017-11-26 01:07:55 -0800

    step 1

--- rocketeer-src/rocketeer_main.c
+++ rocketeer-src/rocketeer_main.c
@@ -12,6 +12,7 @@
 #include <chipmunk/ChipmunkDemoShaderSupport.h>
 #include <spine/spine.h>
 #include <spine/extension.h>
+#include <spine/GLUtils.h>
 #include "spine_bridge.h"
 #include "chipmunk_bridge.h"
 
@@ -36,6 +37,13 @@ static float uvBuffer[8];
 static float bgsScroll[9] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 static cpBody **bodies;
 static int jumped = 0;
+
+
+static unsigned short quadTriangles[6] = {0, 1, 2, 2, 3, 0};
+static spTwoColorBatcher* batcher = 0;
+static spMesh* mesh = 0;
+static spSkeletonClipping* _clipper;
+static float* _worldVertices;
 
 // A single vertex with UV 
 typedef struct Vertex {
@@ -82,7 +90,6 @@ void addVertex(float x, float y, float u, float v, float r, float g, float b, fl
 }
 
 
-
 int impl_draw(int b) {
   //qwqz_bind_frame_buffer(qwqz_engine, b);
   glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -112,11 +119,11 @@ int impl_draw(int b) {
   spAnimationState_apply(state, skeletonTop);
   spSkeleton_updateWorldTransform(skeletonTop);
 
-  glUniform1f(qwqz_engine->m_Linkages[0].g_TimeUniform, qwqz_engine->m_Timers[0].m_SimulationTime);
+  //glUniform1f(qwqz_engine->m_Linkages[0].g_TimeUniform, qwqz_engine->m_Timers[0].m_SimulationTime);
 
-  qwqz_batch_clear(&qwqz_engine->m_Batches[0]);
-  qwqz_engine->m_Batches[0].m_NeedsAttribs = 1;
-  qwqz_batch_prepare(qwqz_engine, &qwqz_engine->m_Batches[0], &qwqz_engine->m_Linkages[0]);
+  //qwqz_batch_clear(&qwqz_engine->m_Batches[0]);
+  //qwqz_engine->m_Batches[0].m_NeedsAttribs = 1;
+  //qwqz_batch_prepare(qwqz_engine, &qwqz_engine->m_Batches[0], &qwqz_engine->m_Linkages[0]);
 
   for (int i=0; i<skeletonTop->slotsCount; i++) {
     spSlot *slot = skeletonTop->drawOrder[i];
@@ -209,7 +216,7 @@ int impl_draw(int b) {
             //// Draw the mesh we created for the attachment
             ////engine_drawMesh(vertices, 0, vertexIndex, texture, engineBlendMode);
             
-            qwqz_batch_add(&qwqz_engine->m_Batches[0], 0, worldVerticesPositions, NULL, regionAttachment->uvs);
+            //qwqz_batch_add(&qwqz_engine->m_Batches[0], 0, worldVerticesPositions, NULL, regionAttachment->uvs);
     } else if (attachment->type == SP_ATTACHMENT_MESH) {
        // Cast to an spMeshAttachment so we can get the rendererObject
        // and compute the world vertices
@@ -243,14 +250,266 @@ int impl_draw(int b) {
        
           //qwqz_batch_add(&qwqz_engine->m_Batches[0], 0, worldVerticesPositions, NULL, mesh->uvs);
        }
-
        //LOGV("%d \n", mesh->trianglesCount);
-      
     }
   }
 
-  glUseProgram(qwqz_engine->m_Linkages[0].m_Program);
-  qwqz_batch_render(qwqz_engine, &qwqz_engine->m_Batches[0]);
+  //glUseProgram(qwqz_engine->m_Linkages[0].m_Program);
+  //qwqz_batch_render(qwqz_engine, &qwqz_engine->m_Batches[0]);
+
+	float* uvs = 0;
+	float* vertices = _worldVertices;
+	int verticesCount = 0;
+	unsigned short* triangles = 0;
+	int trianglesCount = 0;
+	//float r = 0, g = 0, b = 0, a = 0;
+	//float dr = 0, dg = 0, db = 0, da = _premultipliedAlpha ? 1 : 0;
+	for (int i = 0, n = skeletonTop->slotsCount; i < n; i++) {
+		spSlot* slot = skeletonTop->drawOrder[i];
+		if (!slot->attachment) continue;
+		//CCTexture *texture = 0;
+		switch (slot->attachment->type) {
+		case SP_ATTACHMENT_REGION: {
+			spRegionAttachment* attachment = (spRegionAttachment*)slot->attachment;
+			spRegionAttachment_computeWorldVertices(attachment, slot->bone, vertices, 0, 2);
+			//texture = [self getTextureForRegion:attachment];
+			uvs = attachment->uvs;
+			verticesCount = 8;
+			triangles = quadTriangles;
+			trianglesCount = 6;
+			r = attachment->color.r;
+			g = attachment->color.g;
+			b = attachment->color.b;
+			a = attachment->color.a;
+			break;
+		}
+		case SP_ATTACHMENT_MESH: {
+			spMeshAttachment* attachment = (spMeshAttachment*)slot->attachment;
+			spVertexAttachment_computeWorldVertices(SUPER(attachment), slot, 0, attachment->super.worldVerticesLength, vertices, 0, 2);
+			//texture = [self getTextureForMesh:attachment];
+			uvs = attachment->uvs;
+			verticesCount = attachment->super.worldVerticesLength;
+			triangles = attachment->triangles;
+			trianglesCount = attachment->trianglesCount;
+			r = attachment->color.r;
+			g = attachment->color.g;
+			b = attachment->color.b;
+			a = attachment->color.a;
+			break;
+		}
+		case SP_ATTACHMENT_CLIPPING: {
+			spClippingAttachment* clip = (spClippingAttachment*)slot->attachment;
+			spSkeletonClipping_clipStart(_clipper, slot, clip);
+		}
+		default: ;
+		}
+/*	
+		if (texture) {
+			if (slot->data->blendMode != blendMode) {
+				blendMode = slot->data->blendMode;
+				switch (slot->data->blendMode) {
+				case SP_BLEND_MODE_ADDITIVE:
+					[self setBlendMode:[CCBlendMode addMode]];
+					srcBlend = !_premultipliedAlpha ? GL_SRC_ALPHA : GL_ONE;
+					dstBlend = GL_ONE;
+					break;
+				case SP_BLEND_MODE_MULTIPLY:
+					[self setBlendMode:[CCBlendMode multiplyMode]];
+					srcBlend = GL_DST_COLOR;
+					dstBlend = GL_ONE_MINUS_SRC_ALPHA;
+					break;
+				case SP_BLEND_MODE_SCREEN:
+					[self setBlendMode:screenMode];
+					srcBlend = GL_ONE;
+					dstBlend = GL_ONE_MINUS_SRC_COLOR;
+					break;
+				default:
+					[self setBlendMode:_premultipliedAlpha ? [CCBlendMode premultipliedAlphaMode] : [CCBlendMode alphaMode]];
+					srcBlend = !_premultipliedAlpha ? GL_SRC_ALPHA : GL_ONE;
+					dstBlend = GL_ONE_MINUS_SRC_ALPHA;
+				}
+			}
+
+			if (_premultipliedAlpha) {
+				a *= skeletonTop->color.a * slot->color.a;
+				r *= skeletonTop->color.r * slot->color.r * a;
+				g *= skeletonTop->color.g * slot->color.g * a;
+				b *= skeletonTop->color.b * slot->color.b * a;
+			} else {
+				a *= skeletonTop->color.a * slot->color.a;
+				r *= skeletonTop->color.r * slot->color.r;
+				g *= skeletonTop->color.g * slot->color.g;
+				b *= skeletonTop->color.b * slot->color.b;
+			}
+
+			self.texture = texture;
+			CGSize size = texture.contentSize;
+			GLKVector2 center = GLKVector2Make(size.width / 2.0, size.height / 2.0);
+			GLKVector2 extents = GLKVector2Make(size.width / 2.0, size.height / 2.0);
+			if (_skipVisibilityCheck || CCRenderCheckVisbility(transform, center, extents)) {
+			*/
+
+				if (spSkeletonClipping_isClipping(_clipper)) {
+					spSkeletonClipping_clipTriangles(_clipper, vertices, verticesCount, triangles, trianglesCount, uvs, 2);
+					vertices = _clipper->clippedVertices->items;
+					verticesCount = _clipper->clippedVertices->size;
+					uvs = _clipper->clippedUVs->items;
+					triangles = _clipper->clippedTriangles->items;
+					trianglesCount = _clipper->clippedTriangles->size;
+				}
+				
+				if (trianglesCount > 0) {
+					if (!self.twoColorTint) {
+						//CCRenderBuffer buffer = [renderer enqueueTriangles:(trianglesCount / 3) andVertexes:verticesCount withState:self.renderState globalSortOrder:0];
+						for (int i = 0; i * 2 < verticesCount; ++i) {
+							//CCVertex vertex;
+              /*
+							vertex.position = GLKVector4Make(vertices[i * 2], vertices[i * 2 + 1], 0.0, 1.0);
+							vertex.color = GLKVector4Make(r, g, b, a);
+							vertex.texCoord1 = GLKVector2Make(uvs[i * 2], 1 - uvs[i * 2 + 1]);
+							if (_effect) {
+								spColor light;
+								spColor dark;
+								light.r = r;
+								light.g = g;
+								light.b = b;
+								light.a = a;
+								dark.r = dark.g = dark.b = dark.a = 0;
+								_effect->transform(_effect, &vertex.position.x, &vertex.position.y, &vertex.texCoord1.s, &vertex.texCoord1.t, &light, &dark);
+								vertex.color.r = light.r;
+								vertex.color.g = light.g;
+								vertex.color.b = light.b;
+								vertex.color.a = light.a;
+							}
+							CCRenderBufferSetVertex(buffer, i, CCVertexApplyTransform(vertex, transform));
+              */
+						}
+						for (int j = 0; j * 3 < trianglesCount; ++j) {
+							//CCRenderBufferSetTriangle(buffer, j, triangles[j * 3], triangles[j * 3 + 1], triangles[j * 3 + 2]);
+						}
+					} else {
+            /*
+						if (slot->darkColor) {
+							dr = slot->darkColor->r;
+							dg = slot->darkColor->g;
+							db = slot->darkColor->b;
+						} else {
+							dr = dg = db = 0;
+						}
+            */
+						
+						spMeshPart meshPart;
+						spMesh_allocatePart(mesh, &meshPart, verticesCount / 2, trianglesCount, self.texture.name, srcBlend, dstBlend);
+						
+						spVertex* verts = &meshPart.mesh->vertices[meshPart.startVertex];
+						unsigned short* indices = &meshPart.mesh->indices[meshPart.startIndex];
+					
+            /*
+						if (_effect) {
+							spColor light;
+							light.r = r;
+							light.g = g;
+							light.b = b;
+							light.a = a;
+							spColor dark;
+							dark.r = dr;
+							dark.g = dg;
+							dark.b = db;
+							dark.a = da;
+							for (int i = 0; i * 2 < verticesCount; i++, verts++) {
+								spColor lightCopy = light;
+								spColor darkCopy = dark;
+								
+								CCVertex vertex;
+								vertex.position = GLKVector4Make(vertices[i * 2], vertices[i * 2 + 1], 0.0, 1.0);
+								verts->u = uvs[i * 2];
+								verts->v = 1 - uvs[i * 2 + 1];
+								_effect->transform(_effect, &vertex.position.x, &vertex.position.y, &verts->u, &verts->v, &lightCopy, &darkCopy);
+								
+								vertex = CCVertexApplyTransform(vertex, transform);
+								verts->x = vertex.position.x;
+								verts->y = vertex.position.y;
+								verts->z = vertex.position.z;
+								verts->w = vertex.position.w;
+								verts->color = ((unsigned short)(lightCopy.r * 255))| ((unsigned short)(lightCopy.g * 255)) << 8 | ((unsigned short)(lightCopy.b * 255)) <<16 | ((unsigned short)(lightCopy.a * 255)) << 24;
+								verts->color2 = ((unsigned short)(darkCopy.r * 255)) | ((unsigned short)(darkCopy.g * 255)) << 8 | ((unsigned short)(darkCopy.b * 255)) << 16 | ((unsigned short)(darkCopy.a * 255)) << 24;
+								
+							}
+						} else {
+							for (int i = 0; i * 2 < verticesCount; i++, verts++) {
+								CCVertex vertex;
+								vertex.position = GLKVector4Make(vertices[i * 2], vertices[i * 2 + 1], 0.0, 1.0);
+								vertex = CCVertexApplyTransform(vertex, transform);
+								verts->x = vertex.position.x;
+								verts->y = vertex.position.y;
+								verts->z = vertex.position.z;
+								verts->w = vertex.position.w;
+								verts->color = ((unsigned short)(r * 255))| ((unsigned short)(g * 255)) << 8 | ((unsigned short)(b * 255)) <<16 | ((unsigned short)(a * 255)) << 24;
+								verts->color2 = ((unsigned short)(dr * 255)) | ((unsigned short)(dg * 255)) << 8 | ((unsigned short)(db * 255)) << 16 | ((unsigned short)(da * 255)) << 24;
+								verts->u = uvs[i * 2];
+								verts->v = 1 - uvs[i * 2 + 1];
+							}
+						}
+						
+						for (int j = 0; j < trianglesCount; j++, indices++) {
+							*indices = triangles[j];
+						}
+					  
+          */
+
+					  spTwoColorBatcher_add(batcher, meshPart);
+					}
+				}
+			}
+		}
+		spSkeletonClipping_clipEnd(_clipper, slot);
+	}
+	spSkeletonClipping_clipEnd2(_clipper);
+	
+	spTwoColorBatcher_flush(batcher);
+
+  /*
+	if (_debugSlots) {
+		// Slots.
+		CGPoint points[4];
+		for (int i = 0, n = skeletonTop->slotsCount; i < n; i++) {
+			spSlot* slot = skeletonTop->drawOrder[i];
+			if (!slot->attachment || slot->attachment->type != SP_ATTACHMENT_REGION) continue;
+			spRegionAttachment* attachment = (spRegionAttachment*)slot->attachment;
+			spRegionAttachment_computeWorldVertices(attachment, slot->bone, _worldVertices, 0, 2);
+			points[0] = ccp(_worldVertices[0], _worldVertices[1]);
+			points[1] = ccp(_worldVertices[2], _worldVertices[3]);
+			points[2] = ccp(_worldVertices[4], _worldVertices[5]);
+			points[3] = ccp(_worldVertices[6], _worldVertices[7]);
+			[_drawNode drawPolyWithVerts:points count:4 fillColor:[CCColor clearColor] borderWidth:1 borderColor:[CCColor blueColor]];
+		}
+	}
+  */
+
+  /*
+	if (_debugBones) {
+		// Bone lengths.
+		for (int i = 0, n = skeletonTop->bonesCount; i < n; i++) {
+			spBone *bone = skeletonTop->bones[i];
+			float x = bone->data->length * bone->a + bone->worldX;
+			float y = bone->data->length * bone->c + bone->worldY;
+			[_drawNode drawSegmentFrom:ccp(bone->worldX, bone->worldY) to: ccp(x, y)radius:2 color:[CCColor redColor]];
+		}
+		
+		// Bone origins.
+		for (int i = 0, n = skeletonTop->bonesCount; i < n; i++) {
+			spBone *bone = skeletonTop->bones[i];
+			[_drawNode drawDot:ccp(bone->worldX, bone->worldY) radius:4 color:[CCColor greenColor]];
+			if (i == 0) [_drawNode drawDot:ccp(bone->worldX, bone->worldY) radius:4 color:[CCColor blueColor]];
+		}
+	}
+	
+	if (_effect) _effect->end(_effect);
+
+  */
+
+	spMesh_clearParts(mesh);
+
 
   return 0;
 
@@ -379,10 +638,7 @@ int impl_main(int argc, char** argv, GLuint b) {
     }
   }
 
-
   spAtlas* atlas;
-
-  LOGV("ASDASDASDASDASDASDASD\n");
 
   if (doSpine) {
     {
@@ -392,24 +648,14 @@ int impl_main(int argc, char** argv, GLuint b) {
       assert(skeletonData);
       skeletonTop = spSkeleton_create(skeletonData);
       stateData = spAnimationStateData_create(skeletonData);
-      //spAnimationStateData_setMixByName(stateData, "walk_alt", "jump", 0.75);
-      //spAnimationStateData_setMixByName(stateData, "jump", "walk_alt", 0.75);
-
-      //spAnimationStateData_setMixByName(stateData, "walk", "shoot", 0.66);
-      //spAnimationStateData_setMixByName(stateData, "shoot", "walk", 0.66);
 
       state = spAnimationState_create(stateData);
-
-      //spAnimationState_setAnimationByName(state, 0, "walk", 1);
 
       spAnimationState_addAnimationByName(state, 0, "walk", 1, 0.0);
       spAnimationState_addAnimationByName(state, 1, "shoot", 1, 0.0);
 
-      //spAnimationState_addEmptyAnimation(state, 1, 0.33, 0.0);
-      //spAnimationState_addAnimationByName(state, 1, "walk", 1, 0.0);
-
+	    spSkeletonJson_dispose(json);
     }
-
 
     spSkeleton* skeletonFoo = skeletonTop;
 
@@ -419,9 +665,9 @@ int impl_main(int argc, char** argv, GLuint b) {
       "assets/shaders/spine_bone_texture_quad.vsh",
       "assets/shaders/indexed_filled_quad.fsh");
 
-
     int totalVerts = 0;
 
+    /*
     for (int i=0; i<skeletonTop->slotsCount; i++) {
       spSlot *slot = skeletonTop->drawOrder[i];
 
@@ -439,53 +685,34 @@ int impl_main(int argc, char** argv, GLuint b) {
          LOGV("%d \n", mesh->trianglesCount);
       }
     }
+    */
 
-    LOGV("------ %d %d !!!!!\n",  skeletonTop->slotsCount, totalVerts);
+    //LOGV("------ %d %d !!!!!\n",  skeletonTop->slotsCount, totalVerts);
+    //qwqz_batch_init(&qwqz_engine->m_Batches[0], &qwqz_engine->m_Linkages[0], 11);
 
-    qwqz_batch_init(&qwqz_engine->m_Batches[0], &qwqz_engine->m_Linkages[0], 11);
+    //glUseProgram(qwqz_engine->m_Linkages[0].m_Program);
+    //int roboRegionRenderObject = (int)atlas->rendererObject; //(int)((spAtlasRegion *)((spRegionAttachment *)skeleton->drawOrder[0]->attachment)->rendererObject)->page->rendererObject; //TODO: fix this, fuck yea C
+    
+    ////LOGV("wtf %d!!!!!!\n\n\n\n", roboRegionRenderObject);
 
-    glUseProgram(qwqz_engine->m_Linkages[0].m_Program);
-    int roboRegionRenderObject = (int)atlas->rendererObject; //(int)((spAtlasRegion *)((spRegionAttachment *)skeleton->drawOrder[0]->attachment)->rendererObject)->page->rendererObject; //TODO: fix this, fuck yea C
+    //glUniform1i(qwqz_engine->m_Linkages[0].g_TextureUniform, roboRegionRenderObject); //TODO: texture unit
 
-    LOGV("wtf %d!!!!!!\n\n\n\n", roboRegionRenderObject);
+    //g_TextureOffset = glGetUniformLocation(program, "iTextureOffset");
+    //glUniform2f(g_TextureOffset, 0, 0);
 
-    glUniform1i(qwqz_engine->m_Linkages[0].g_TextureUniform, roboRegionRenderObject); //TODO: texture unit
+    //qwqz_engine->g_lastFrameBuffer = b;
+    //qwqz_engine->m_Zoom2 = 512.0 / 1.0;
 
-    g_TextureOffset = glGetUniformLocation(program, "iTextureOffset");
-    glUniform2f(g_TextureOffset, 0, 0);
+    //glActiveTexture(GL_TEXTURE0);
 
-    qwqz_engine->g_lastFrameBuffer = b;
-    qwqz_engine->m_Zoom2 = 512.0 / 1.0;
+    //spSkeleton_updateWorldTransform(skeletonTop);
 
-    glActiveTexture(GL_TEXTURE0);
-
-    spSkeleton_updateWorldTransform(skeletonTop);
-
-    if (0) {
-      bodies = (cpBody **)malloc(sizeof(cpBody *) * skeletonTop->slotsCount);
-
-      for (int i=0; i<skeletonTop->slotsCount; i++) {
-        spSlot *s = skeletonTop->drawOrder[i];
-        if (s->attachment && s->attachment->type == SP_ATTACHMENT_REGION) {
-          spRegionAttachment *ra = (spRegionAttachment *)s->attachment;
-
-
-          cpBody *body;
-          cpShape *shape;
-
-          body = cpBodyNew(INFINITY, cpMomentForBox(INFINITY, ra->width, ra->height));
-          body->userData = (void *)1;
-          bodies[i] = body;
-
-
-          shape = cpSpaceAddShape(space, cpBoxShapeNew(body, ra->width, ra->height, 15.0f));
-          cpShapeSetElasticity(shape, 0.0f);
-          cpShapeSetFriction(shape, 1.0f);
-          cpGroup spineGroup = 2;
-          shape->filter.group = spineGroup;
-        }
-      }
+    if (!batcher) {
+      batcher = spTwoColorBatcher_create();
+      mesh = spMesh_create(64000, 32000);
     }
+	
+	  _worldVertices = MALLOC(float, 1000); // Max number of vertices per mesh.
   }
 
   return 0;
